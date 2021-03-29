b385371278232c108d466b9f37fffe02be58e6ff
Pavel Sekishov <pasha-sveta@yandex.ru>
2021-03-29 19:16:21 +0300

    Correct-2

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-console.log('Hi')
+new WOW().init();
+
+/************* Anchor ***************/
+$(window).scroll(function(){
+    if($(this).scrollTop() > 400){
+        $('#anchor').fadeIn();
+    }else{
+        $('#anchor').fadeOut();
+    }
+
+});
+
+$('#anchor').click(function(){
+    $('body, html').animate({
+        scrollTop:0
+    }, 900);
+    return false;
+});
